29a8cbfebbc09595bc84fa8b7bbea602b0ebca07
Sam Roelants <roelants.sam@gmail.com>
2018-03-16 15:29:26 +0100

    Added config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js'
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'); 
+        path: path.resolve(__dirname, 'dist') 
         // npm path module resolves this into an absolute path
     }
 };
