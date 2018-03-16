efd4d4dff7f4783b8705b4a39758257b504ef98a
Sam Roelants <roelants.sam@gmail.com>
2018-03-16 17:04:21 +0100

    Output Management

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js'
+        print: './src/print.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist') 
         // npm path module resolves this into an absolute path
-    },
-    module: {
-        rules: [
-          {
-              test: /\.css$/, use: ['style-loader', 'css-loader']
-          },
-          {
-              test:/\.(png|svg|jpg|gif)$/, use: ['file-loader'] 
-          },
-          {
-              test:/\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader']
-          },
-          {
-              test:/\.(csv|tsv)$/, use: ['csv-loader']
-          },
-          {
-              test:/\.xml$/, use: ['xml-loader']
-          }
-        ]
     }
 };
