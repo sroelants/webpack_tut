9e97c4f84f5912d17ee541f3bac84117eb77aff1
Sam Roelants <roelants.sam@gmail.com>
2018-03-16 16:01:39 +0100

    Added file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
         rules: [
           {
               test: /\.css$/, use: ['style-loader', 'css-loader']
+          },
+          {
+              test:/\.(png|svg|jpg|gif)$/, use: ['file-loader'] 
           }
         ]
     }
